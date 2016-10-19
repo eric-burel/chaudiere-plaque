e1969503422311d98e38c959c1afa25ed85fd07b
Eric Burel <e.burel.contact@gmail.com>
2016-10-19 13:21:34 +0200

    Added react-meteor-data to link reactive vars and component
    To avoid redundancy, Signup should be made stateless, and handled by a bigger User component

--- client/users/components/SignupForm.jsx
+++ client/users/components/SignupForm.jsx
@@ -1,9 +1,12 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
+import { Meteor } from 'meteor/meteor'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 
-export default class SignupForm extends Component {
-
+// TODO : use a global Users component instead, which print the correct form
+// depending on the route and inject this kind of data
+class SignupForm extends Component {
   constructor(props){
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this) // manual binding
@@ -13,49 +16,81 @@ export default class SignupForm extends Component {
     event.preventDefault() // prevent browser submition
     const email = this.refs.email.input.value.trim() // email is a TextField, input.value is the corresponding text
     const password = this.refs.password.input.value.trim()
-    // TODO : create the account
+    Meteor.call('user.signup', email, password, (err, userId)=>{
+      if (err){
+        console.log(err)
+      } else {
+        console.log(userId)
+        // then we login the user
+        Meteor.loginWithPassword(email, password, (err)=>{
+          if (err){
+            console.log(err)
+          } else {
+            console.log("here")
+            console.log(Meteor.userId())
+          }
+        })
+      }
+    })
   }
+
+
   render(){
+    let formOrLogout = ''
+    if (!this.props.userId){
+      formOrLogout =
+      <form ref="signupForm" onSubmit={this.handleSubmit}>
+        <div className="row">
+          <div className="col-xs-12">
+            <TextField
+              floatingLabelText="Email"
+              ref="email"
+              type="email"
+              fullWidth={true}
+              />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <TextField
+              floatingLabelText="Mot de passe"
+              ref="password"
+              type="password"
+              fullWidth={true}
+              />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <RaisedButton
+              label="S'inscrire"
+              primary={true}
+              fullWidth={true}
+              onClick={this.handleSubmit}
+              />
+          </div>
+        </div>
+      </form>
+    } else {
+      formOrLogout =
+      <p>Vous êtes déjà connecté.</p>
+    }
+
     return(
       <div>
         <p><strong>Inscription</strong></p>
-        <form ref="signupForm" onSubmit={this.handleSubmit}>
-          <div className="row">
-            <div className="col-xs-12">
-              <TextField
-                floatingLabelText="Email"
-                ref="email"
-                type="email"
-                fullWidth={true}
-                />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12">
-              <TextField
-                floatingLabelText="Mot de passe"
-                ref="password"
-                type="password"
-                fullWidth={true}
-                />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12">
-              <RaisedButton
-                label="S'inscrire"
-                primary={true}
-                fullWidth={true}
-                onClick={this.handleSubmit}
-                />
-            </div>
-          </div>
-        </form>
+        {formOrLogout}
       </div>
     )
   }
 
 }
+// Meteor data container
+export default createContainer(() => {
+  return {
+    userId: Meteor.userId()
+  };
+}, SignupForm);
 /* Example from React doc :
 
 class MyForm extends React.Component {
