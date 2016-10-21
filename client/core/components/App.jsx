516fe20dbc0a2d47242608cc80863fed8864d730
Eric Burel <e.burel.contact@gmail.com>
2016-10-21 09:15:11 +0200

    Loading injectTapEvent during Meteor startup
    This guarantees that the module is loaded, eventhough the App component is not
    (for example when there is a Discover or Home component for non-connected users)

--- client/core/components/App.jsx
+++ client/core/components/App.jsx
@@ -2,10 +2,6 @@ import React, { Component, PropTypes } from 'react';
 
 // material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
 // material-ui components
 import RaisedButton from 'material-ui/RaisedButton'
 import AppBar from 'material-ui/AppBar'
--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,11 @@ import { render } from 'react-dom'
 
 import App from './core/components/App.jsx'*/
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 Meteor.startup(() => {
   /*render(
     <App />,
