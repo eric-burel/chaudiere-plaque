08003f4f71d6621fd68ba00d921d436c516472e4
Eric Burel <e.burel.contact@gmail.com>
2016-10-31 20:40:32 +0100

    Refactored the Menu
    The MenuBar is the bar itself, whereas the MenuDrawer is the left menu

--- client/core/components/App.jsx
+++ client/core/components/App.jsx
@@ -1,15 +1,11 @@
 import React, { Component, PropTypes } from 'react';
-
 // material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 // material-ui components
-import RaisedButton from 'material-ui/RaisedButton'
-import AppBar from 'material-ui/AppBar'
 // core components
 import Header from './Header'
-import Menu from './Menu'
+import MenuBar from './MenuBar'
 import Footer from './Footer'
-import Link from './Link'
 import Users from '../../users/components/Users'
 
 
@@ -21,22 +17,6 @@ export default class App extends Component {
     this.title = "Chaudière Plaque"
   }
 
-  getLinks() {
-    const newLink = (label, routeName, external)=>{
-      return {key:routeName, label, routeName, external}
-    }
-    return [
-      newLink('Meteor guide','https://guide.meteor.com/',true),
-      newLink('React guide','https://facebook.github.io/react/docs/getting-started.html',true),
-      newLink('Flexbox Grid','http://flexboxgrid.com/',true),
-      newLink('Material-ui guide','http://www.material-ui.com/#/components/app-bar',true),
-      newLink('Radium CSS','https://formidable.com/open-source/radium/docs/api',true),
-      newLink('Enzyme (React testing)','http://airbnb.io/enzyme/docs/guides.html',true),
-      newLink('Mocha (test framework)','https://mochajs.org/',true),
-      newLink('Chai (assertion)', 'http://chaijs.com/api/assert/',true),
-      newLink('Sinon (spy, stubs and mock)', 'http://sinonjs.org/docs/',true)
-    ];
-  }
 
 
   // NOTE: this.props.content is handled by the router
@@ -45,9 +25,7 @@ export default class App extends Component {
       <MuiThemeProvider>
         <div>
           <Header/>
-          <Menu
-            links={this.getLinks()}
-            />
+          <MenuBar />
           <div className="row">
             <div className="col-xs-12">
               {this.props.content}
---
+++ client/core/components/MenuDrawer.jsx
@@ -0,0 +1,96 @@
+/**
+ * Left menu that appears when clicking the menu icon
+ */
+import React, {Component, PropTypes} from 'react'
+import createRouteListener from '../factories/createRouteListener'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import Divider from 'material-ui/Divider'
+import Subheader from 'material-ui/Subheader'
+import {Menu, MenuItem } from 'material-ui/Menu'
+import {FlowRouter} from 'meteor/kadira:flow-router'
+
+class MenuDrawer extends Component {
+  constructor(){
+    super()
+    const newLink = (label, routeName, external)=>{
+      return {key:routeName, label, routeName, external}
+    }
+    this.externalLinks =  [
+      newLink('Meteor guide','https://guide.meteor.com/',true),
+      newLink('React guide','https://facebook.github.io/react/docs/getting-started.html',true),
+      newLink('Flexbox Grid','http://flexboxgrid.com/',true),
+      newLink('Material-ui guide','http://www.material-ui.com/#/components/app-bar',true),
+      newLink('Radium CSS','https://formidable.com/open-source/radium/docs/api',true),
+      newLink('Enzyme (React testing)','http://airbnb.io/enzyme/docs/guides.html',true),
+      newLink('Mocha (test framework)','https://mochajs.org/',true),
+      newLink('Chai (assertion)', 'http://chaijs.com/api/assert/',true),
+      newLink('Sinon (spy, stubs and mock)', 'http://sinonjs.org/docs/',true)
+    ]
+  }
+
+  buildOnClick(link){
+    if (link.external){
+      return ()=>{window.open(link.routeName, '_blank')} // external links are opend in a new tab/window
+    } else {
+      return ()=>{FlowRouter.go(link.routeName)}
+    }
+  }
+
+  renderLinks(links){
+    return links.map((link)=>{
+      return(
+        <MenuItem
+          key={link.routeName}
+          value={link.routeName}
+          primaryText={link.label}
+          onClick={this.buildOnClick(link)}
+          />
+      )
+    })
+  }
+
+  renderDrawerMenu(){
+    return(
+      <div>
+        <AppBar title="Menu" showMenuIconButton={false}
+          iconClassNameRight="fa fa-chevron-left"
+          onRightIconButtonTouchTap={()=>{this.props.toggle()}}/>
+        <Subheader>External links</Subheader>
+        <Menu value={this.props.currentRoute}>
+          {this.renderLinks(this.externalLinks)}
+        </Menu>
+        <Divider></Divider>
+      </div>
+    )
+  }
+
+
+  render(){
+    return (
+      <Drawer
+        docked={false}
+        open={this.props.open}
+        onRequestChange={this.props.onRequestChange}
+        >
+        {this.renderDrawerMenu()}
+      </Drawer>
+    )
+  }
+}
+export default createRouteListener(MenuDrawer)
+
+MenuDrawer.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onRequestChange : PropTypes.func.isRequired,
+  toggle : PropTypes.func.isRequired,
+  //@see https://facebook.github.io/react/docs/typechecking-with-proptypes.html
+  /*links: PropTypes.arrayOf(PropTypes.shape({
+  routeName: PropTypes.string.isRequired,
+  external: PropTypes.bool,
+  label : PropTypes.string.isRequired,
+  })),*/
+  currentRoute : PropTypes.object.isRequired // passed by Meteor
+
+
+}
