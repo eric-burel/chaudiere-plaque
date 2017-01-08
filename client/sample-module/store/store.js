192c4c21f19e4f8f8607eff07393c9cf16fa2248
Eric Burel <e.burel.contact@gmail.com>
2017-01-08 21:01:29 +0100

    Review of the folder structure

--- client/sample-module/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-// TODO : add the logger middleware
-import sampleApp from '../reducers/reducers'
-const store = createStore(sampleApp,
-  applyMiddleware(thunkMiddleware)
-)
-export default store
--- scripts/unit-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Executes Meteor unit tests using the mocha driver
-meteor test  --driver-package practicalmeteor:mocha
-
-# Use --settings to load custom settings during tests
-#meteor test --settings settings.json --driver-package practicalmeteor:mocha
