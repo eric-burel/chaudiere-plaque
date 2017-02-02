5bd54bbd2e4de6c6b0749e53de3aec341cdc6707
Eric Burel <e.burel.contact@gmail.com>
2017-02-02 22:39:21 +0100

    Added files

---
+++ core/client/main.js
@@ -0,0 +1,7 @@
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+Meteor.startup(() => {
+})
