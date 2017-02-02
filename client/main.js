1d9d02e5b7f3a8567247c565803f09a6eb861c70
Eric Burel <e.burel.contact@gmail.com>
2017-02-02 22:38:30 +0100

    Refactored code with a module architecture

--- client/main.js
+++
@@ -1,7 +0,0 @@
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
-
-Meteor.startup(() => {
-})
